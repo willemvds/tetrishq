ee17024d06972986be43deca79d4acb76f1115ef
Willem van der Schyff <willemvds@gmail.com>
2024-03-01 06:54:15 +0200

    feat: Plug introspect example from guide.

--- lib/tetrishq_web/endpoint.ex
+++ lib/tetrishq_web/endpoint.ex
@@ -44,5 +44,16 @@ defmodule TetrishqWeb.Endpoint do
   plug Plug.MethodOverride
   plug Plug.Head
   plug Plug.Session, @session_options
+  plug :introspect
   plug TetrishqWeb.Router
+
+  def introspect(conn, _opts) do
+    IO.puts """
+    Verb: #{inspect(conn.method)}
+    Host: #{inspect(conn.host)}
+    Headers: #{inspect(conn.req_headers)}
+    """
+
+    conn
+  end
 end
