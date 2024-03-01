a5e94c556b6d38e01112e9f4d85809d764977085
Willem van der Schyff <willemvds@gmail.com>
2024-03-01 07:02:27 +0200

    feat: Plug locale module example from guide.

--- lib/tetrishq_web/router.ex
+++ lib/tetrishq_web/router.ex
@@ -8,6 +8,7 @@ defmodule TetrishqWeb.Router do
     plug :put_root_layout, html: {TetrishqWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug TetrishqWeb.Plugs.Locale, "en"
   end
 
   pipeline :api do
