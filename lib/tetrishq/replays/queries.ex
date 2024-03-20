45b63ace7d1f8bc7e6618c23598d6a5d49c3dcfc
Willem van der Schyff <willemvds@gmail.com>
2024-03-20 17:08:30 +0200

    feat: Start tinkering with erlexec to get recording stats from tetris.

--- lib/tetrishq/replays/queries.ex
+++ lib/tetrishq/replays/queries.ex
@@ -9,4 +9,19 @@ defmodule TetrisHQ.Replays.Queries do
 
     TetrisHQ.Repo.all(query)
   end
+
+  def recording_stats(recording_path) do
+    recording_stats_result =
+      :exec.run("tetris recording-stats #{recording_path}", [:sync, :stdout])
+
+    case recording_stats_result do
+      {:ok, recording_stats_json} ->
+        IO.inspect(recording_stats_json)
+        {:ok, recording_stats_json}
+
+      {:error, err} ->
+        IO.inspect(err)
+        {:error, err}
+    end
+  end
 end
--- mix.exs
+++ mix.exs
@@ -47,7 +47,8 @@ defmodule TetrisHQ.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.2"}
+      {:bandit, "~> 1.2"},
+      {:erlexec, "~> 2.0"}
     ]
   end
 
